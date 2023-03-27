8c73fc2e29224fe735ef439acc5ff8de3815f80f
Jyoti <jyoti.duwal@outcodesoftware.com>
2023-03-27 14:23:37 +0545

    Fixed total price issue.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,8 +5,10 @@ import "./Layout.css";
 import CartItems from "./CartItems";
 import {useSelector} from "react-redux";
 const Layout = () => {
-  let total = 100;
   const shouldShowCart = useSelector((state) => state.cart.showCart)
+  const cartItems = useSelector((state) => state.cart.itemsList)
+  console.log(cartItems)
+  const totalPrice = cartItems.reduce((accumulator, iterator ) => iterator.totalPrice + accumulator, 0);
 
   return (
     <React.Fragment>
@@ -15,7 +17,7 @@ const Layout = () => {
         <Products />
         { shouldShowCart && <CartItems />}
         <div className="total-price">
-          <h3>Total: ${total}</h3>
+          <h3>Total: ${totalPrice}</h3>
           <button className="orderBtn">Place Order</button>
         </div>{" "}
       </div>
